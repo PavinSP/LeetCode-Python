b18a780c87589a2fab893d356ba16163430c93f0
PavinSP <pavinsp122002@gmail.com>
2025-01-12 18:54:53 +0530

    Solution 2 and 3

--- Problem 0001/Solution2.py
+++ Problem 0001/Solution2.py
@@ -1,16 +1,13 @@
-# Time Complexity: O(n^2)
-# Space Complexity: O(1)
-
-class Solution:
+class Solution2:
     def twoSum(self, nums, target):
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i != j:
-                    if nums[i] + nums[j] == target:
-                        return [i, j]
+            for j in range(i+1, len(nums)):
+                if i + j == target:
+                    return [i, j]
+        return []
 
 def main():
-    solution = Solution()
+    solution = Solution2()
     nums = [2, 7, 11, 15]
     target = 9
     result = solution.twoSum(nums, target)
---
+++ Problem 0001/Solution3.py
@@ -0,0 +1,26 @@
+# Two pass hash table
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution2:
+    def twoSum(self, nums, target):
+        map = {}
+        for i in range(len(nums)):
+            map[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in map and map[complement] != i:
+                return [i, map[complement]]
+        return []
+
+
+def main():
+    solution = Solution2()
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = solution.twoSum(nums, target)
+    print(f"Input: nums = {nums}, target = {target}")
+    print(f"Output: {result}")
+
+if __name__ == "__main__":
+    main()
