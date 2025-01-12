93ed3e0248120d7e7136d61a1e3fcb260f1423b2
PavinSP <pavinsp122002@gmail.com>
2025-01-12 18:59:53 +0530

    Changes

--- Problem 0001/Solution2.py
+++ Problem 0001/Solution2.py
@@ -1,4 +1,4 @@
-class Solution2:
+class Solution:
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
@@ -7,7 +7,7 @@ class Solution2:
         return []
 
 def main():
-    solution = Solution2()
+    solution = Solution()
     nums = [2, 7, 11, 15]
     target = 9
     result = solution.twoSum(nums, target)
--- Problem 0001/Solution3.py
+++ Problem 0001/Solution3.py
@@ -2,7 +2,7 @@
 # Time Complexity: O(n)
 # Space Complexity: O(n)
 
-class Solution2:
+class Solution:
     def twoSum(self, nums, target):
         map = {}
         for i in range(len(nums)):
@@ -15,7 +15,7 @@ class Solution2:
 
 
 def main():
-    solution = Solution2()
+    solution = Solution()
     nums = [2, 7, 11, 15]
     target = 9
     result = solution.twoSum(nums, target)
